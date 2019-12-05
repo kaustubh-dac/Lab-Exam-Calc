8cc64e95f7da51195b5364242be59d206901d802
elpidaguy <kaustubh.joshi136@gmail.com>
2019-12-05 14:47:32 +0000

    Updated Calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,38 +1,41 @@
 #include<iostream>
+#include<exception>
+#include<cstdlib>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 int ch=0;
 int a=0, b=0;
-bool flag = 1;
 
-while(flag)
-{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 
-cout<<"Enter value for a: "<<endl;
-cin>>a;
-cout<<"Enter Value for b: "<<endl;
-cin>>b;
-
-cout<<"MENU"<<endl;
-cout<<"1.Addition"<<endl;
-cout<<"6.Exit"<<endl;
-cout<<"Enter Your Choice: ";
-cin>>ch;
-
+ch = atoi(argv[3]);
+cout<<ch;
 switch(ch)
 {
 
 case 1:
 	cout<<"Addition of a and b is: "<<a+b<<endl;
 	break;
-case 6:
-	flag=0;
-	break;
+case 2:
+    cout<<"Subtraction of a and b is: "<<a-b<<endl;
+    break;
+case 3:
+    cout<<"Multiplication of a and b is: "<<a*b<<endl;
+    break;
+case 4:
+    try{
+        cout<<"Division of a and b is: "<<a/b<<endl;
+    }
+    catch(exception e)
+    {
+        cout<<e.what();
+    }
+    break;
 }
 
-}
 
 return 0;
 
